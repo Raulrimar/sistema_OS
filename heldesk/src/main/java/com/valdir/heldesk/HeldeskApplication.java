ae57021d87baa943cdbc58bd14bf856f13d0dbe3
Raurimar <digisofthumaita@gmail.com>
2024-09-13 10:53:31 -0500

    Crianção de Frontend para o Sistema de OS

---
+++ heldesk/src/main/java/com/valdir/heldesk/HeldeskApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.heldesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeldeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeldeskApplication.class, args);
+	}
+
+}
---
+++ heldesk/src/test/java/com/valdir/heldesk/HeldeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdir.heldesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeldeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
